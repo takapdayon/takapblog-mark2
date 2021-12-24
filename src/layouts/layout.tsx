f73d8f4cb79e1f2e08efa32caad1a315806b3452
takapdayon <business.takap@gmail.com>
2021-12-25 03:31:01 +0900

    update: css moduleを使用しないように

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,41 +1,46 @@
 import PropTypes from 'prop-types';
-import * as React from 'react'
-import { Link } from 'gatsby'
-import { useSiteMetadata } from '../utils/hooks'
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-  siteTitle,
-} from './layout.module.css'
+import * as React from 'react';
+import { Link } from 'gatsby';
+import { useSiteMetadata } from '../utils/hooks';
+import Footer from '../components/footer';
+import Header from '../components/header';
 
 
 const Layout: React.VFC<Props> = ({ pageTitle, children }) => {
   const { site } = useSiteMetadata()
   return (
-    <div className={container}>
-      <title>{pageTitle} | {site?.siteMetadata?.title}</title>
-      <header className={siteTitle}>{site?.siteMetadata?.title}</header>
+    <div>
+      <Header />
+      <header>{site?.siteMetadata?.title}</header>
       <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
+        <ul>
+          <li>
+            <Link to="/">
               Home
             </Link>
           </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
+          <li>
+            <Link to="/about">
               About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/tag">
+              Tag
             </Link>
           </li>
         </ul>
       </nav>
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
+        <h1>{pageTitle}</h1>
         {children}
       </main>
+      <Footer />
     </div>
   )
 }
