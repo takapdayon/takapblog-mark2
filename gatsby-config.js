426fb70ddcb96fa5719772d45e1d7d7e05dc528c
takapdayon <business.takap@gmail.com>
2022-05-17 00:21:46 +0900

    add: ESlint, Prittier周り設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,18 @@ module.exports = {
         path: `${__dirname}/content/images`,
       }
     },
+    {
+      resolve: `gatsby-plugin-eslint`,
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    }
     /*
     {
       resolve: `gatsby-plugin-google-analytics`,
